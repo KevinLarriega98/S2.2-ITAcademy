c49fef08fffe6c95d955919fe123fe12b9652c65
KevinLarriega98 <kevinlarriega@gmail.com>
2025-07-21 10:43:49 +0200

    Exercise 5 completed

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,3 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 const products = [
     {
         id: 1,
@@ -64,15 +63,9 @@ const products = [
     }
 ]
 
-// => Reminder, it's extremely important that you debug your code. 
-// ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
-// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
+const cart = []
 
-// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-const cart = [];
-
-const total = 0;
+const total = 0
 
 // Exercise 1
 const buy = (id) => {
@@ -91,6 +84,11 @@ const buy = (id) => {
     }
 
     console.log(cart)
+
+    applyPromotionsCart()
+    printCart()
+
+    document.getElementById('count_product').textContent = cart.length
 }
 
 document.querySelectorAll('.add-to-cart').forEach(button => {
@@ -138,10 +136,31 @@ const applyPromotionsCart = () => {
 }
 
 // Exercise 5
-const printCart = () => {
-    // Fill the shopping cart modal manipulating the shopping cart dom
-}
+function printCart() {
+    const cartList = document.getElementById("cart_list")
+    const totalPriceSpan = document.getElementById("total_price")
 
+    cartList.innerHTML = ""
+
+    let total = 0
+
+    cart.forEach(product => {
+        const tr = document.createElement("tr")
+        const subtotal = product.subtotalWithDiscount || (product.price * product.quantity)
+        total += subtotal
+
+        tr.innerHTML = `
+            <th scope="row">${product.name}</th>
+            <td>$${product.price}</td>
+            <td>${product.quantity}</td>
+            <td>$${subtotal.toFixed(2)}</td>
+        `
+
+        cartList.appendChild(tr)
+    })
+
+    totalPriceSpan.textContent = total.toFixed(2)
+}
 
 // ** Nivell II **
 
@@ -151,5 +170,5 @@ const removeFromCart = (id) => {
 }
 
 const open_modal = () => {
-    printCart();
+    printCart()
 }
